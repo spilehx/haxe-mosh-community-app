459422d033f95ccc9bb4ff82060105f657d29ded
jonnyAnd <jonny@eino.co.uk>
2026-08-31 05:28:06 +0100

    heaps base

--- src/Main.hx
+++ src/Main.hx
@@ -1,28 +1,14 @@
 import js.Browser;
 
-class Main {
-	static function main() {
-		var document = Browser.document;
-		var root = document.getElementById("app");
+class Main extends hxd.App {
+    override function init() {
+        var text = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
+        text.text = "Hello game";
+        text.x = 100;
+        text.y = 100;
+    }
 
-		if (root == null) {
-			trace("Missing #app element");
-			return;
-		}
-
-		root.innerHTML = "";
-
-		var card = document.createDivElement();
-		card.className = "card";
-
-		var title = document.createElement("h1");
-		title.textContent = "HAXE-MOSH";
-
-		var message = document.createParagraphElement();
-		message.textContent = "Let's make something together!";
-
-		card.appendChild(title);
-		card.appendChild(message);
-		root.appendChild(card);
-	}
+    static function main() {
+        new Main();
+    }
 }
